617a91510fce0545813e2f6fc4f17c0c9567d502
Andros Serrano Hernandez <andros@infomail.upr.edu.cu>
2014-06-09 13:04:21 -0700

    cambiado lo de form

--- src/Fao/MainBundle/Controller/DocsAdminController.php
+++ src/Fao/MainBundle/Controller/DocsAdminController.php
@@ -13,6 +13,7 @@ class DocsAdminController extends CRUD{
 
     public function createAction()
     {
+        // the key used to lookup the template
         $templateKey = 'edit';
 
         if (false === $this->admin->isGranted('CREATE')) {
@@ -22,21 +23,29 @@ class DocsAdminController extends CRUD{
         $object = $this->admin->getNewInstance();
 
         $this->admin->setSubject($object);
+
+        //esto es mio
         $securityContext = $this->get('security.context');
         $adminId = $securityContext->getToken()->getUser();
-        /** @var $form \Symfony\Component\Form\Form */
         $object->setUser($adminId);
         $object->setFechaDePublicacion(new \DateTime('now'));
+
+        /** @var $form \Symfony\Component\Form\Form */
         $form = $this->admin->getForm();
         $form->setData($object);
 
         if ($this->getRestMethod()== 'POST') {
-            $form->bind($this->get('request'));
+            $form->submit($this->get('request'));
 
             $isFormValid = $form->isValid();
 
             // persist if the form was valid and if in preview mode the preview was approved
             if ($isFormValid && (!$this->isInPreviewMode() || $this->isPreviewApproved())) {
+
+                if (false === $this->admin->isGranted('CREATE', $object)) {
+                    throw new AccessDeniedException();
+                }
+
                 $this->admin->create($object);
 
                 if ($this->isXmlHttpRequest()) {
@@ -46,7 +55,8 @@ class DocsAdminController extends CRUD{
                     ));
                 }
 
-                $this->addFlash('sonata_flash_success','flash_create_success');
+                $this->addFlash('sonata_flash_success', $this->admin->trans('flash_create_success', array('%name%' => $this->admin->toString($object)), 'SonataAdminBundle'));
+
                 // redirect to edit mode
                 return $this->redirectTo($object);
             }
@@ -54,7 +64,7 @@ class DocsAdminController extends CRUD{
             // show an error message if the form failed validation
             if (!$isFormValid) {
                 if (!$this->isXmlHttpRequest()) {
-                    $this->addFlash('sonata_flash_error', 'flash_create_error');
+                    $this->addFlash('sonata_flash_error', $this->admin->trans('flash_create_error', array('%name%' => $this->admin->toString($object)), 'SonataAdminBundle'));
                 }
             } elseif ($this->isPreviewRequested()) {
                 // pick the preview template if the form was valid and preview was requested
@@ -70,6 +80,92 @@ class DocsAdminController extends CRUD{
 
         return $this->render($this->admin->getTemplate($templateKey), array(
             'action' => 'create',
+            'form'   => $view,
+            'object' => $object,
+        ));
+    }
+
+    public function editAction($id = null)
+    {
+        // the key used to lookup the template
+        $templateKey = 'edit';
+
+        $id = $this->get('request')->get($this->admin->getIdParameter());
+        $object = $this->admin->getObject($id);
+
+        if (!$object) {
+            throw new NotFoundHttpException(sprintf('unable to find the object with id : %s', $id));
+        }
+
+        if (false === $this->admin->isGranted('EDIT', $object)) {
+            throw new AccessDeniedException();
+        }
+
+        $this->admin->setSubject($object);
+
+        //esto es mio tambien
+        if($this->admin->isGranted('ROLE_EDITOR'))
+        {
+            $form = $this->admin->getForm()
+                ->add('estado', 'choice',
+                    array('choices' => array('Publicado' => 'Publicar',
+                        'Presentado' => 'Presentado'), 'required' => true, 'empty_value' => 'Seleccione un Valor',
+                        'empty_data'  => null, 'label' => 'Estado', ));
+            $form->setData($object);
+        }
+        else
+        {
+            $form = $this->admin->getForm();
+            $form->setData($object);
+        }
+        if($this->admin->isGranted('ROLE_EDITOR') && $object->getEstado() == 'Publicado')
+            $object->setFechaDePublicacion(new \DateTime('now'));
+
+        /** @var $form \Symfony\Component\Form\Form */
+        //$form = $this->admin->getForm();
+        //$form->setData($object);
+
+        if ($this->getRestMethod() == 'POST') {
+            $form->submit($this->get('request'));
+
+            $isFormValid = $form->isValid();
+
+            // persist if the form was valid and if in preview mode the preview was approved
+            if ($isFormValid && (!$this->isInPreviewMode() || $this->isPreviewApproved())) {
+                $this->admin->update($object);
+
+                if ($this->isXmlHttpRequest()) {
+                    return $this->renderJson(array(
+                        'result'    => 'ok',
+                        'objectId'  => $this->admin->getNormalizedIdentifier($object)
+                    ));
+                }
+
+                $this->addFlash('sonata_flash_success', $this->admin->trans('flash_edit_success', array('%name%' => $this->admin->toString($object)), 'SonataAdminBundle'));
+
+                // redirect to edit mode
+                return $this->redirectTo($object);
+            }
+
+            // show an error message if the form failed validation
+            if (!$isFormValid) {
+                if (!$this->isXmlHttpRequest()) {
+                    $this->addFlash('sonata_flash_error', $this->admin->trans('flash_edit_error', array('%name%' => $this->admin->toString($object)), 'SonataAdminBundle'));
+                }
+            } elseif ($this->isPreviewRequested()) {
+                // enable the preview template if the form was valid and preview was requested
+                $templateKey = 'preview';
+                $this->admin->getShow();
+            }
+        }
+
+        $view = $form->createView();
+
+        // set the theme for the current Admin Form
+        $this->get('twig')->getExtension('form')->renderer->setTheme($view, $this->admin->getFormTheme());
+
+        return $this->render($this->admin->getTemplate($templateKey), array(
+            'action' => 'edit',
             'form'   => $view,
             'object' => $object,
         ));
