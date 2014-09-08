4fbf948d85154fec465f015fb933bec0b54d7877
Andros Serrano Hernandez <andros@infomail.upr.edu.cu>
2014-09-07 18:10:13 -0700

    footer1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,6 +35,7 @@ class AppKernel extends Kernel
             // KNP HELPER BUNDLES
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
 
             // USER
             new FOS\UserBundle\FOSUserBundle(),
