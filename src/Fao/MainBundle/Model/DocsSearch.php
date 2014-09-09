9b8de7a1ae5cdf8d13adbd4458909746e032fe79
Rafael Ernesto Ferro <rafix81@gmail.com>
2014-09-09 11:07:02 -0400

    index only docs published

---
+++ src/Fao/MainBundle/Model/DocsSearch.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rafix
+ * Date: 9/09/14
+ * Time: 10:42
+ */
+
+namespace Fao\MainBundle\Model;
+
+
+use Symfony\Component\HttpFoundation\Request;
+
+class DocsSearch {
+
+} 
